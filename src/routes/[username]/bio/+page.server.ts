7f054830ee50e24c1eaf38f145db6d062c566a3a
davidthus <dvdnicorici05@gmail.com>
2023-10-21 23:11:45 +0100

    add bio form actions

--- src/routes/[username]/bio/+page.server.ts
+++ src/routes/[username]/bio/+page.server.ts
@@ -1,5 +1,5 @@
 import { adminDB } from '$lib/server/admin'
-import { error, redirect } from '@sveltejs/kit'
+import { error, fail, redirect } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
 export const load = (async ({ locals, params }) => {
@@ -20,3 +20,27 @@ export const load = (async ({ locals, params }) => {
     bio
   }
 }) satisfies PageServerLoad
+
+export const actions = {
+  default: async (locals, request, params) => {
+    const uid = locals.userID
+
+    const data = await request.formData()
+    const bio = data.get('bio')
+
+    const userRef = adminDB.collection('users').doc(uid!)
+    const { username } = (await userRef.get()).data()!
+
+    if (username !== params.username) {
+      throw error(401, 'This aint your profile bud')
+    }
+
+    if (bio!.length > 260) {
+      return fail(400, { problem: 'Bio must be less than 260 characters' })
+    }
+
+    await userRef.update({
+      bio
+    })
+  }
+}
--- src/routes/[username]/bio/+page.svelte
+++ src/routes/[username]/bio/+page.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
+  import { enhance } from '$app/forms'
+  import { page } from '$app/stores'
   import type { PageData } from './$types'
 
   export let data: PageData
 </script>
 
-<h1>Edit Bio</h1>
+<main class="max-w-lg prose text-center mx-auto my-6" use:enhance>
+  <p>Current Bio: <span class="text-info">{data.bio}</span></p>
+  <p>Status Code: {$page.status}</p>
+  <p class="text-error">{$page.form.problem ?? ''}</p>
+  <form action="" method="post">
+    <div class="form-control">
+      <label for="bio" class="label">
+        <span class="label-text">Your bio</span>
+      </label>
+      <textarea
+        name="bio"
+        value={data.bio}
+        class="textarea textarea-bordered textarea-accent"
+      />
+    </div>
+    <button class="btn btn-primary my-5">Update bio</button>
+  </form>
+</main>
