c5e64ffaa07c04d53f329356cf93c094b7f3fa33
davidthus <dvdnicorici05@gmail.com>
2023-10-22 23:35:37 +0100

    fix hooks.server.ts errors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,16 @@
-import type { Handle } from "@sveltejs/kit";
+import { adminAuth } from '$lib/server/admin'
+import type { Handle } from '@sveltejs/kit'
 
-export const handle: Handle = (async ({event, resolve})) => {
+export const handle: Handle = async ({ event, resolve }) => {
   const sessionCookie = event.cookies.get('__session')
 
   try {
     const decodedClaims = await adminAuth.verifySessionCookie(sessionCookie!)
-    event.locals.userID = decodedClaims.uid;
+    event.locals.userID = decodedClaims.uid
     console.log('found user id', decodedClaims.uid)
-  } catch(e){
-    event.locals.userID = null;
-    return resolve(event);
+  } catch (e) {
+    event.locals.userID = null
+    return resolve(event)
   }
   return resolve(event)
 }
